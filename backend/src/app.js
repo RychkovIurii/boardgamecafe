0dd0ba11b7b41a56da1467d8457684d6c8f712c0
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-01-14 00:19:34 +0200

    Changed names for .env. Changed structure for clarity.

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+//Set up the main Express application
+
+const express = require('express');
+const dotenv = require('dotenv');
+const routes = require('../routes');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000', // Allow only specific frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true, // Allow cookies or Authorization headers
+}));
+
+app.use(express.json());
+
+// Test Route
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+// Routes
+app.use('/api', routes);
+
+module.exports = app;
