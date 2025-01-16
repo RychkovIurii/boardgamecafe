83d76d4d6068556551e76efce546366431da8c46
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-01-16 22:54:10 +0200

    Added Payment scheme.

---
+++ backend/models/Payment.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+    bookingId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Booking', // References the Booking model
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: [true, 'Payment amount is required'],
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'completed', 'failed'],
+        default: 'pending',
+    },
+    paymentMethod: {
+        type: String,
+        enum: ['credit_card', 'paypal', 'cash'],
+        required: [true, 'Payment method is required'],
+    },
+    transactionId: {
+        type: String,
+        required: [true, 'Transaction ID is required'],
+        unique: true,
+    },
+	stripePaymentIntentId: {
+        type: String,
+        required: [true, 'Stripe Payment Intent ID is required'],
+        unique: true,
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model('Payment', paymentSchema);
