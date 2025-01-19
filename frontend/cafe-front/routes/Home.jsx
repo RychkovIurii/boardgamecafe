b2b176f417b3e64371bafe344b98f80aef0e3d31
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-01-19 23:31:50 +0200

    Found some bug with navbar. Doesn't render as soon I import it.

--- frontend/cafe-front/routes/Home.jsx
+++ frontend/cafe-front/routes/Home.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import heroImage from '../assets/hero_home.jpg';
 import Hero from '../components/Hero';
-import Navbar from '../components/Navbar';
+/* import Navbar from '../components/Navbar'; */
 import Footer from '../components/Footer';
 
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-            <Navbar/>
+            {/* <Navbar /> */}
             <Hero
-            cName = "hero"
-            heroImage = {heroImage}
-            title = "Welcome to BoardGameCafe"
-            text = "Enjoy the best board games with your friends and family."
-            linkText = "Book Now"
-            linkClass = "show"
-            url = "/bookings"
+                cName="hero"
+                heroImage={heroImage}
+                title="Welcome to BoardGameCafe"
+                text="Enjoy the best board games with your friends and family."
+                linkText="Book Now"
+                linkClass="show"
+                url="/bookings"
             />
-            <Footer/>
+            <Footer />
         </>
-    )
+    );
 }
 
 export default Home;
