00c162c115f95020ffa736e610d19af038290d32
Sephrodite <132352821+Sephrodite@users.noreply.github.com>
2025-02-06 10:34:00 +0200

    hero switch
    changed the hero for a cafe made image.

--- frontend/cafe-front/routes/Bookings.jsx
+++ frontend/cafe-front/routes/Bookings.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import heroImage from '../assets/hero_home.jpg';
+import heroImage from '../assets/hero_booking.png';
 import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
