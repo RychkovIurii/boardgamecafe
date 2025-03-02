15bba4b986e967b287295276e35fc0d99dc54b5f
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-03-02 21:53:48 +0200

    Edit booking draft. Need to set up validations.

--- frontend/cafe-front/src/App.jsx
+++ frontend/cafe-front/src/App.jsx
@@ -6,13 +6,14 @@ import FAQ from '../routes/FAQ';
 import Games from '../routes/Games';
 import Events from '../routes/Events';
 import SignIn from '../routes/SignIn';
-import AdminDashboard from '../routes/AdminDashboard';
+import AdminDashboard from '../routes/admin/AdminDashboard';
 import { AuthContext } from '../context/AuthContext';
 import { Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import Contact from '../routes/Contact';
 import './App.css'
 import Bookings from '../routes/Bookings';
+import EditBooking from '../routes/admin/EditBooking';
 
 function App() {
 
@@ -32,6 +33,7 @@ function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/bookings' element={<Bookings />} />
         <Route path='/admin' element={<AdminRoute element={<AdminDashboard />} />} />
+		<Route path="/admin/edit-booking/:id" element={<AdminRoute element={<EditBooking />} />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
     </div>
