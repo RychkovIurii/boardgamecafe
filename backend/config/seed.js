f06075ed4e7187557e90c5b60a8df8d16690adc0
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-03-10 15:09:29 +0200

    Added events to DB and seed.

--- backend/config/seed.js
+++ backend/config/seed.js
@@ -5,6 +5,7 @@ const Game = require('../models/Game');
 const Booking = require('../models/Booking');
 const Payment = require('../models/Payment');
 const Table = require('../models/Table');
+const Event = require('../models/Event');
 const moment = require('moment-timezone');
 
 dotenv.config({ path: './back.env' });
@@ -85,6 +86,26 @@ const seedTables = [
     { number: 27, capacity: 4, location: "terrace" }
 ];
 
+const seedEvents = [
+	{
+	  title: 'Miniature Painting Workshop',
+	  description: 'Learn new techniques for painting miniatures. Paints and brushes provided. Bring your own miniatures!',
+	  date: new Date('2025-04-25T18:00:00'),
+	  image: 'https://taverncraftstudios.com/cdn/shop/articles/q_a_how_to_print_3d_resin_miniatures.jpg?v=1700578830&width=1100'
+	},
+	{
+	  title: 'Dungeon Master Workshop',
+	  description: 'Master the art of game mastering with experienced GMs. Learn to create engaging campaigns for your players.',
+	  date: new Date('2025-05-05T16:00:00'),
+	  image: 'https://www.estywaygaming.com/cdn/shop/products/dungeonmaster.png?v=1706421135'
+	},
+	{
+	  title: 'Murder Mystery Night',
+	  description: 'Solve a thrilling murder mystery while enjoying drinks and snacks. Can you find the killer?',
+	  date: new Date('2025-03-14T19:30:00'),
+	  image: 'https://murdermysteryevents.com/wp-content/uploads/2023/08/What-is-Murder-Mystery.jpg'
+	}
+  ];
 
 const seedBookings = [
     {
@@ -137,6 +158,7 @@ const importData = async () => {
         await Booking.deleteMany();
         await Payment.deleteMany();
         await Table.deleteMany();
+		await Event.deleteMany();
 
         const createdUsers = await Promise.all(seedUsers.map(user => User.create(user)));
         const createdGames = await Game.insertMany(seedGames);
@@ -173,6 +195,9 @@ const importData = async () => {
 		});
 		
 		await Promise.all(createdBookings.map(booking => Booking.findByIdAndUpdate(booking._id, { paymentId: booking.paymentId })));
+
+		const createdEvents = await Event.insertMany(seedEvents);
+		console.log(`${createdEvents.length} events added`);
 
         console.log('Data Imported!');
         process.exit();
---
+++ backend/models/Event.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Event', eventSchema);
