8763f07f6fc040d28514953c5f2f98276b3d4645
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-03-10 21:13:25 +0200

    Add padding and margin for Table Reservation.

--- frontend/cafe-front/routes/Bookings.jsx
+++ frontend/cafe-front/routes/Bookings.jsx
@@ -4,15 +4,15 @@ import Footer from '../components/Footer';
 import BookingForm from '../components/BookingForm copy';
 
 export default function Bookings() {
-    return (
-        <>
-            <Navbar />
-            <div>
-                <h1>Table Reservation</h1>
-                {/* <img class="floorplan" src={floorplan}></img> */}
-                <BookingForm/>
-            </div>
-            <Footer />
-        </>
-    )
+	return (
+		<>
+			<Navbar />
+			<div>
+				<h1 style={{ padding: '20px', marginTop: '20px' }}>Table Reservation</h1>
+				{/* <img class="floorplan" src={floorplan}></img> */}
+				<BookingForm/>
+			</div>
+			<Footer />
+		</>
+	)
 }
