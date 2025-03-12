105ac76f04a338e0e9657a75e56e6fb0f3488001
Sephrodite <132352821+Sephrodite@users.noreply.github.com>
2025-03-12 23:23:29 +0200

    added games list
    includes basic styling and generating of the items through the BBG API. Integration of said API and conversion from XML to a workable format for react.

---
+++ frontend/cafe-front/components/GameCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../components/Style/GameCardStyles.css'
+
+
+export default function GameCard({ gamesInfo }) {
+    return (
+
+        <div className='gameBG'>
+            <div className='cardContent'>
+                <div className='gameTit'>
+                    {gamesInfo.title}
+                </div>
+                <div className='imgHold'>
+                    <img className='gameImg' src={gamesInfo.image} />
+                </div>
+            </div>
+        </div>
+
+    )
+}
--- frontend/cafe-front/routes/Games.jsx
+++ frontend/cafe-front/routes/Games.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import heroImage from '../assets/gamesHero.png';
 import Hero from '../components/Hero';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { useEffect } from 'react';
+import GameCard from '../components/GameCard';
+import '../components/Style/GameCardStyles.css'
+/* import usePagination from '@mui/material/usePagination';
+import Stack from '@mui/material/Stack'; */
+
 
 function Games() {
+    const [gamesInfo, setGamesInfo] = useState([])
+    useEffect(() => {
+        fetch("https://boardgamegeek.com/xmlapi2/collection?username=Cafebg")
+            .then((response) => response.text())
+            .then((xmlString) => {
+                const parser = new DOMParser();
+                const xml = parser.parseFromString(xmlString, "text/xml");
+                const items = xml.getElementsByTagName("item");
+                const itemArray = Array.from(items).map((itemNode) => {
+                    const objectId = itemNode.getAttribute("objectid");
+
+                    const name = itemNode.querySelector("name");
+                    const image = itemNode.querySelector("image");
+                    const thumb = itemNode.querySelector("thumbnail");
+                    const year = itemNode.querySelector("yearpublished");
+
+                    return {
+                        id: objectId,
+                        title: name ? name.textContent : "",
+                        image: image ? image.textContent.trim() : "",
+                        thumbnail: thumb ? thumb.textContent.trim() : "",
+                        year: year ? year.textContent : ""
+                    };
+
+                });
+                setGamesInfo(itemArray);
+            })
+    }, []);
+
+
     return (
         <>
             <Navbar />
@@ -17,7 +53,14 @@ function Games() {
                 linkClass="show"
                 url="/bookings"
             />
-			<h1>Here we will have some games?</h1>
+            <h1>The Collection of Games in Cafe Boardgame:</h1>
+            
+            <div className='gameGen'>
+                {gamesInfo.map(gamesInfo =>
+                    <GameCard key={gamesInfo.id} gamesInfo={gamesInfo} />
+                )}
+            </div>
+
             <Footer />
         </>
     );
