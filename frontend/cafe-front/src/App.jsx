6eb5254af407657a720db87184faaeef0ba02ca9
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-03-17 14:12:58 +0200

    Added EditTables page adn routes.

--- frontend/cafe-front/src/App.jsx
+++ frontend/cafe-front/src/App.jsx
@@ -15,6 +15,7 @@ import './App.css'
 import Bookings from '../routes/Bookings';
 import EditBooking from '../routes/admin/EditBooking';
 import EditEvents from '../routes/admin/EditEvents';
+import EditTables from '../routes/admin/EditTables';
 import EditPricing from '../routes/admin/EditPricing';
 import Service from '../routes/Service';
 import ServiceProduct from '../routes/ServiceProduct';
@@ -40,6 +41,7 @@ function App() {
         <Route path='/admin' element={<AdminRoute element={<AdminDashboard />} />} />
 		<Route path='/admin/edit-events' element={<AdminRoute element={<EditEvents />} />} />
 		<Route path='/admin/edit-pricing' element={<AdminRoute element={<EditPricing />} />} />
+		<Route path='/admin/edit-tables' element={<AdminRoute element={<EditTables />} />} />
         <Route path="/admin/edit-booking/:id" element={<AdminRoute element={<EditBooking />} />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/service' element={<Service />} />
