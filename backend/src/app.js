4a99349cde38ee9605a308a5b7cec35fc0bf74a9
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-03-25 23:04:22 +0200

    Removed unused vars. Server and app refactoring.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,31 +1,23 @@
-//Set up the main Express application
-
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-dotenv.config({ path: './back.env' });
 
+dotenv.config({ path: './back.env' });
 
 const routes = require('../routes');
 const app = express();
+
 app.use(cookieParser());
-
-
-app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Allow only specific frontend URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-    credentials: true, // Allow cookies or Authorization headers
-}));
-
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-/* // Test Route
-app.get('/', (req, res) => {
-    res.send('API is running...');
-}); */
-
-// Routes
 app.use('/', routes);
 
 module.exports = app;
