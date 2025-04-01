dccfde6f4d7cc2ef33ea96e468263428bfe935ff
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-04-01 15:52:53 +0300

    Login fix.

--- frontend/cafe-front/components/Login.jsx
+++ frontend/cafe-front/components/Login.jsx
@@ -26,6 +26,7 @@ const Login = ({ onToggleForm }) => {
 			/* const { role } = response.data; */ //For cookie-based authentication.
 			const { token, role } = response.data;
 			localStorage.setItem('accessToken', token);
+			login(token);
 			await Swal.fire({
 				icon: 'success',
 				title: t("login.successTitle"),
