eca44ca8019376250446db9a30a5930a071cfeea
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-04-01 10:49:57 +0300

    Fixed Menu img.

---
+++ frontend/cafe-front/src/assets/icons/icon.js
@@ -0,0 +1,15 @@
+import beer from './beer.png';
+import pizza from './pizza.png';
+import softdrink from './softdrink.png';
+import hotdrink from './hotdrink.png';
+import board_game_icon from './board_game_icon.png';
+import logo from './logo.png';
+
+export const iconMap = {
+  'beer.png': beer,
+  'pizza.png': pizza,
+  'softdrink.png': softdrink,
+  'hotdrink.png': hotdrink,
+  'board_game_icon.png': board_game_icon,
+  'logo.png': logo
+};
