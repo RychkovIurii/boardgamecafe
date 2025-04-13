bb4219e051793190571d87643db530ca43e8c737
Rychkov Iurii <rychkoviurii@hotmail.com>
2025-04-13 12:23:44 +0300

    Added color for Swal buttons.

--- frontend/cafe-front/routes/MyBookings.jsx
+++ frontend/cafe-front/routes/MyBookings.jsx
@@ -245,8 +245,6 @@ const MyBookings = () => {
                 text: t('bookingForm.deleteConfirmText'),
                 icon: 'warning',
                 showCancelButton: true,
-                confirmButtonColor: '#d33',
-                cancelButtonColor: '#3085d6',
                 confirmButtonText: t('bookingForm.deleteConfirmButton'),
                 cancelButtonText: t('bookingForm.cancelButtonText')
             });
--- frontend/cafe-front/routes/admin/AdminDashboard.jsx
+++ frontend/cafe-front/routes/admin/AdminDashboard.jsx
@@ -70,8 +70,6 @@ const AdminDashboard = () => {
                 text: t('adminDashboard.deleteConfirm.text'),
                 icon: 'warning',
                 showCancelButton: true,
-                confirmButtonColor: '#d33',
-                cancelButtonColor: '#3085d6',
                 confirmButtonText: t('adminDashboard.deleteConfirm.confirmButton'),
                 cancelButtonText: t('adminDashboard.deleteConfirm.cancelButton')
             });
