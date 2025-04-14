b1745bdf9771ef7f5105d758da99b0223c17afbc
Nasrin-MT <155952085+Nasrin-MT@users.noreply.github.com>
2025-04-14 13:15:19 +0300

    Applied colors to Hero.

--- frontend/cafe-front/components/Hero.jsx
+++ frontend/cafe-front/components/Hero.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import "./Style/HeroStyles.css";
+import { colors } from '../components/Style/Colors';
+//import { fonts } from '../components/Style/Fonts';
 
 function Hero(props) {
   const { t } = useTranslation();
@@ -11,10 +13,10 @@ function Hero(props) {
     <div className={cName}>
       <img className="imgClass" alt="HeroImg" src={heroImage} />
       {(title || text || linkText) && (
-        <div className="hero-text">
+        <div className="hero-text" style={{ color: colors.color.fontHeroTitle }}>
           {title && <h1>{t(`hero.${title}`)}</h1>}
           {text && (
-            <div className="hero-text-content">
+            <div className="hero-text-content" style={{ color: colors.color.fontHeroSubTitle}}>
               {typeof text === 'string' ? t(`hero.${text}`) : text}
             </div>
           )}
