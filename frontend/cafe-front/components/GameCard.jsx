4080caaeb82616fbf9d833b52f40f238466063b8
Rychkov Iurii <rychkoviurii@hotmail.com>
2025-04-16 11:01:05 +0300

    Switched fallback image in gameCards from relative to import for proper building.

--- frontend/cafe-front/components/GameCard.jsx
+++ frontend/cafe-front/components/GameCard.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import '../components/Style/GameCardStyles.css'
+import React from 'react';
+import '../components/Style/GameCardStyles.css';
 import { colors } from '../components/Style/Colors';
+import fallbackImage from '../src/assets/icons/boardgame_icom_round.png';
 
 export default function GameCard({ gamesInfo }) {
     return (
-
         <div className='gameBG' style={{ backgroundColor: colors.color.CardBgColor }}>
             <div className='cardContent'>
                 <div className='gameTit' style={{ color: colors.color.fontTitle }}>
                     {gamesInfo.title}
                 </div>
                 <div className='imgHold'>
-                    <img className='gameImg' src={gamesInfo.image} alt={gamesInfo.title} 
-                    onError={(e) => {
-                        e.target.onerror = null; // prevents infinite loop
-                        e.target.src = '../src/assets/icons/boardgame_icom_round.png'; // fallback image
-                      }}/>
+                    <img
+                        className='gameImg'
+                        src={gamesInfo.image}
+                        alt={gamesInfo.title}
+                        onError={(e) => {
+                            e.target.onerror = null; // prevents infinite loop
+                            e.target.src = fallbackImage; // Use the imported fallback image
+                        }}
+                    />
                 </div>
             </div>
         </div>
-
-    )
+    );
 }
