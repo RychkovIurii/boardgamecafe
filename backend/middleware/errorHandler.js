56fd768a743a6b1557d2ca1d21a99f5fc75beabd
Iurii Rychkov <rychkoviurii@hotmail.com>
2025-09-18 15:26:07 +0200

    Implement CSRF protection middleware and update error handling for invalid tokens

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,7 +1,11 @@
 const errorHandler = (err, req, res, next) => { // I have to think about this
+	if (err.code === 'EBADCSRFTOKEN') {
+		return res.status(403).json({ message: 'Invalid CSRF token' });
+	}
+
 	// Default to a 500 Internal Server Error if no status code has been set
 	const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
-  
+
 	res.status(statusCode);
 	res.json({
 	  message: err.message,
